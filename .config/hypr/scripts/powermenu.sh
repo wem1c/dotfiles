c90b24634f4f9ccdca079f3e726bbc022086911a
Conor C. Peterson <conorpetersondev@gmail.com>
2023-12-12 00:21:50 +0100

    feat(powermenu): add exit option

--- .config/hypr/scripts/powermenu.sh
+++ .config/hypr/scripts/powermenu.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-entries="Reboot
+entries="Exit
+Reboot
 Shutdown"
 
 selected=$(printf '%s\n' "$entries" | wofi -S dmenu | awk '{print tolower($1)}')
 
 case $selected in
+exit)
+  hyprctl dispatch exit
+  ;;
 reboot)
   exec systemctl reboot
   ;;
