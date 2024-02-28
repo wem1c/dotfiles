d77ee7cbdf0be77021b1eca2559dafd2e1b5524a
Conor C. Peterson <conorpetersondev@gmail.com>
2024-02-28 13:40:53 +0100

    fix(scripts/touchpad_toggle): temporarily replace hyprctl notify with notify-send

--- .config/hypr/scripts/touchpad_toggle.sh
+++ .config/hypr/scripts/touchpad_toggle.sh
@@ -3,6 +3,7 @@
 # Set device to be toggled
 HYPRLAND_DEVICE="elan1300:00-04f3:3087-touchpad"
 HYPRLAND_VARIABLE="device:$HYPRLAND_DEVICE:enabled"
+ICON_INFO="/usr/share/icons/Adwaita/symbolic/status/dialog-information-symbolic.svg"
 
 if [ -z "$XDG_RUNTIME_DIR" ]; then
   export XDG_RUNTIME_DIR=/run/user/$(id -u)
@@ -13,10 +14,20 @@ DEVICE="$(hyprctl getoption $HYPRLAND_VARIABLE | grep 'int: 1')"
 
 if [ -z "$DEVICE" ]; then
 	# if the device is disabled, then enable
-  	hyprctl notify 1 2500 0 "  Enabling touchpad..."
+
+	# BUG: cannot set fontsize for hyprctl-notify
+	# See: https://github.com/hyprwm/Hyprland/issues/4866
+  	#hyprctl notify 1 2500 0 "  Enabling touchpad..."
+  	notify-send -u low -t 2500 -i $ICON_INFO -e "Enabling touchpad..."
+  	
 	hyprctl keyword $HYPRLAND_VARIABLE true
 else
 	# if the device is enabled, then disable
-	hyprctl notify 1 2500 0 "  Disabling touchpad..."
+
+	# BUG: cannot set fontsize for hyprctl-notify
+	# See: https://github.com/hyprwm/Hyprland/issues/4866
+	#hyprctl notify 1 2500 0 "  Disabling touchpad..."
+	notify-send -u low -t 2500 -i $ICON_INFO -e "Disabling touchpad..."
+
 	hyprctl keyword $HYPRLAND_VARIABLE false
 fi
