b9692de079a9a1c158c1502fad58a60e3e20b621
Conor C. Peterson <conorpetersondev@gmail.com>
2024-02-28 14:21:07 +0100

    refactor(scripts/touchpad_toggle): add notify_user() function

--- .config/hypr/scripts/touchpad_toggle.sh
+++ .config/hypr/scripts/touchpad_toggle.sh
@@ -5,12 +5,17 @@ HYPRLAND_DEVICE="elan1300:00-04f3:3087-touchpad"
 HYPRLAND_VARIABLE="device:$HYPRLAND_DEVICE:enabled"
 ICON_INFO="/usr/share/icons/Adwaita/symbolic/status/dialog-information-symbolic.svg"
 
-if [ -z "$XDG_RUNTIME_DIR" ]; then
-  export XDG_RUNTIME_DIR=/run/user/$(id -u)
-fi
-
 # Check if device is currently enabled (1 = enabled, 0 = disabled)
 DEVICE="$(hyprctl getoption $HYPRLAND_VARIABLE | grep 'int: 1')"
+
+#######################################
+# Send notification containing current brightness value.
+# Arguments:
+#   Notification message.
+#######################################
+notify_user() {
+	notify-send -u low -t 2500 -i $ICON_INFO -e "$1"
+}
 
 if [ -z "$DEVICE" ]; then
 	# if the device is disabled, then enable
@@ -18,7 +23,7 @@ if [ -z "$DEVICE" ]; then
 	# BUG: cannot set fontsize for hyprctl-notify
 	# See: https://github.com/hyprwm/Hyprland/issues/4866
   	#hyprctl notify 1 2500 0 "  Enabling touchpad..."
-  	notify-send -u low -t 2500 -i $ICON_INFO -e "Enabling touchpad..."
+  	notify_user "Enabling touchpad..."
   	
 	hyprctl keyword $HYPRLAND_VARIABLE true
 else
@@ -27,7 +32,7 @@ else
 	# BUG: cannot set fontsize for hyprctl-notify
 	# See: https://github.com/hyprwm/Hyprland/issues/4866
 	#hyprctl notify 1 2500 0 "  Disabling touchpad..."
-	notify-send -u low -t 2500 -i $ICON_INFO -e "Disabling touchpad..."
-
+	notify_user "Disabling touchpad..."
+	
 	hyprctl keyword $HYPRLAND_VARIABLE false
 fi
